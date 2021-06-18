7e26870b38d9f4ccb5411bbca998128ab7f2ce3a
Dmitry Prokoptsev <dprokoptsev@gmail.com>
2021-06-18 00:05:27 +0000

    Initial commit.

---
+++ manage.py
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+""":"
+DIR="$(dirname "$0")"
+if [ -r "$DIR/venv/bin/python" ]; then
+    PYTHON="$DIR/venv/bin/python3"
+else
+    PYTHON=python3
+fi
+exec "$PYTHON" "$0" "$@"
+"""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitecfg.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
